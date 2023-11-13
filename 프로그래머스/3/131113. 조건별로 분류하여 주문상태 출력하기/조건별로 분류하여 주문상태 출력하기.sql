0b876e0a394351e91494770ddb1ff97a1492f9b1
DecimalToBinary <76907818+DecimalToBinary@users.noreply.github.com>
2023-11-13 23:04:00 +0900

    [level 3] Title: 조건별로 분류하여 주문상태 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/131113. 조건별로 분류하여 주문상태 출력하기/조건별로 분류하여 주문상태 출력하기.sql
@@ -0,0 +1,10 @@
+-- 코드를 입력하세요
+# DATEDIFF()와 TIMESTAMPDIFF()의 사용 >> 매개변수, 반환값 ??
+# 기준날짜1-기준날짜2
+# CASE WHEN 용법 아는가? IF 문이랑 헷갈리진 않았는지?
+SELECT ORDER_ID, PRODUCT_ID, DATE_FORMAT(OUT_DATE,'%Y-%m-%d') AS OUT_DATE,
+CASE WHEN OUT_DATE IS NULL THEN '출고미정' 
+WHEN OUT_DATE<='2022-05-01' THEN '출고완료'
+ELSE '출고대기' END AS 출고여부
+FROM FOOD_ORDER
+ORDER BY ORDER_ID ASC
